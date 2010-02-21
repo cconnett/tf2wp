ecee95a6346f27079c1a966d5b9449998bbc14f1
Chris Connett <chris@connett.net>
2010-02-21 01:52:33 -0500

    First commit of the TF2 Win Probability Calculator.

---
+++ backend.php
@@ -0,0 +1,85 @@
+<?php
+$db = mysql_pconnect("mysql.connett.net", "alphachannel", "smackdog");
+if (!$db) {
+  die('Could not connect to database: '. mysql_error());
+ }
+mysql_select_db("tf2wp");
+
+$point = $_GET["point"];
+$attackers = "Red";
+$defenders = "Blue";
+$o = "r";
+$d = "b";
+if ($point < 3) {
+  $point = (6 - $point);
+  $attackers = "Blue";
+  $defenders = "Red";
+  $o = "b";
+  $d = "r";
+ }
+
+$arr = array("fightsWon" => 0, "roundsWon" => 0, "sampleSize" => 0);
+$query = "select fightsWon, roundsWon, sampleSize " .
+  "from wpSummary " .
+  "where o1 = '" . mysql_real_escape_string($_GET[$o . "1"]) .
+  "' and o2 = '" . mysql_real_escape_string($_GET[$o . "2"]) .
+  "' and o4 = '" . mysql_real_escape_string($_GET[$o . "4"]) .
+  "' and o7 = '" . mysql_real_escape_string($_GET[$o . "7"]) .
+  "' and oc = '" . mysql_real_escape_string($_GET[$o . "c"]) .
+  "' and d1 = '" . mysql_real_escape_string($_GET[$d . "1"]) .
+  "' and d2 = '" . mysql_real_escape_string($_GET[$d . "2"]) .
+  "' and d4 = '" . mysql_real_escape_string($_GET[$d . "4"]) .
+  "' and d7 = '" . mysql_real_escape_string($_GET[$d . "7"]) .
+  "' and dc = '" . mysql_real_escape_string($_GET[$d . "c"]) .
+  "' and map like '" . mysql_real_escape_string($_GET["map"]) .
+  "' and point = '" . mysql_real_escape_string($point) . "'";
+  $result = mysql_query($query, $db);
+
+if ($result) {
+  $row = mysql_fetch_array($result);
+  $arr["fightsWon"] += $row["fightsWon"];
+  $arr["roundsWon"] += $row["roundsWon"];
+  $arr["sampleSize"] += $row["sampleSize"];
+ }
+
+if ($arr["sampleSize"]) {
+  $fwp = (int)round($arr["fightsWon"] / $arr["sampleSize"] * 100, 0);
+  $rwp = (int)round($arr["roundsWon"] / $arr["sampleSize"] * 100, 0);
+  $moe = (int)round(0.82/sqrt($arr["sampleSize"]) * 100, 0);
+  if ($moe == 0) {
+    $moe = "< 0.5";
+  }
+  //$moe = $arr["sampleSize"];
+} else {
+  $fwp = "No data";
+  $rwp = "No data";
+  $moe = "No data";
+}
+?>
+<tbody>
+  <tr>
+    <td class="label">P(<?php print $attackers ?> Caps):</td>
+    <td class="wp"><?php print $fwp; ?>%</td>
+    <td class="moe">&plusmn;<?php print $moe;?>%</td>
+  </tr>
+  <tr id="secondrow">
+    <td class="label">P(<?php print $attackers ?> Wins Round):</td>
+    <td class="wp"><?php print $rwp;?>%</td>
+    <td class="moe">&plusmn;<?php print $moe;?>%</td>
+  </tr>
+</tbody>
+<tbody>
+  <tr>
+    <td class="label">P(<?php print $defenders ?> Caps):</td>
+    <td class="wp"><?php print 100-$fwp;?>%</td>
+    <td class="moe">&plusmn;<?php print $moe;?>%</td>
+  </tr>
+  <tr>
+    <td class="label">P(<?php print $defenders ?> Wins Round):</td>
+    <td class="wp">
+  <?php print 100-$rwp;?>%</td>
+    <td class="moe">&plusmn;<?php print $moe;?>%</td>
+  </tr>
+</tbody>
+
+<?php mysql_close($db); ?>
---
+++ code.js
@@ -0,0 +1,160 @@
+var gray = 'gray';
+var red = '#ab3738';
+var blue = '#466578';
+var point = 3;
+
+function point1() {
+    document.all.cp1.style.backgroundColor = red;
+    document.all.cp2.style.backgroundColor = blue;
+    document.all.cp3.style.backgroundColor = blue;
+    document.all.cp4.style.backgroundColor = blue;
+    document.all.cp5.style.backgroundColor = blue;
+    point = 1;
+    updateWP();
+}
+function point2() {
+    document.all.cp1.style.backgroundColor = red;
+    document.all.cp2.style.backgroundColor = red;
+    document.all.cp3.style.backgroundColor = blue;
+    document.all.cp4.style.backgroundColor = blue;
+    document.all.cp5.style.backgroundColor = blue;
+    point = 2;
+    updateWP();
+}
+function point3() {
+    document.all.cp1.style.backgroundColor = red;
+    document.all.cp2.style.backgroundColor = red;
+    document.all.cp3.style.backgroundColor = gray;
+    document.all.cp4.style.backgroundColor = blue;
+    document.all.cp5.style.backgroundColor = blue;
+    point = 3;
+    updateWP();
+}
+function point4() {
+    document.all.cp1.style.backgroundColor = red;
+    document.all.cp2.style.backgroundColor = red;
+    document.all.cp3.style.backgroundColor = red;
+    document.all.cp4.style.backgroundColor = blue;
+    document.all.cp5.style.backgroundColor = blue;
+    point = 4;
+    updateWP();
+}
+function point5() {
+    document.all.cp1.style.backgroundColor = red;
+    document.all.cp2.style.backgroundColor = red;
+    document.all.cp3.style.backgroundColor = red;
+    document.all.cp4.style.backgroundColor = red;
+    document.all.cp5.style.backgroundColor = blue;
+    point = 5;
+    updateWP();
+}
+
+var players = [];
+players["r1"] = 2;
+players["r2"] = 2;
+players["r4"] = 1;
+players["r7"] = 1;
+players["rc"] = 0;
+
+players["b1"] = 2;
+players["b2"] = 2;
+players["b4"] = 1;
+players["b7"] = 1;
+players["bc"] = 0;
+
+function killPlayer(tag) {
+    document.getElementById(tag + '-on').style.visibility = 'hidden';
+    document.getElementById(tag + '-off').style.visibility = 'visible';
+    document.getElementById(tag + '-x').style.visibility = 'visible';
+    document.getElementById(tag).onclick = function() {revivePlayer(tag);};
+    players[tag.substr(0,2)] -= 1;
+
+    if (tag.charAt(1) == '7') {
+        document.getElementById(tag.charAt(0) + 'c-on').style.visibility = 'hidden';
+        document.getElementById(tag.charAt(0) + 'c-off').style.visibility = 'hidden';
+
+        // Don't force the uber off here.  Do it when sending the XHR,
+        // so the UI maintains uber state.
+
+        //players[tag.charAt(0) + 'c'] = 0;
+    }
+    updateWP();
+}
+function revivePlayer(tag) {
+    document.getElementById(tag + '-on').style.visibility = 'visible';
+    document.getElementById(tag + '-off').style.visibility = 'hidden';
+    document.getElementById(tag + '-x').style.visibility = 'hidden';
+    document.getElementById(tag).onclick = function() {killPlayer(tag);};
+    players[tag.substr(0,2)] += 1;
+
+    if (tag.charAt(1) == '7') {
+        document.getElementById(tag.charAt(0) + 'c-on').style.visibility = 'hidden';
+        document.getElementById(tag.charAt(0) + 'c-off').style.visibility = 'hidden';
+        if (players[tag.charAt(0) + 'c'] > 0) {
+            document.getElementById(tag.charAt(0) + 'c-on').style.visibility = 'visible';
+        } else {
+            document.getElementById(tag.charAt(0) + 'c-off').style.visibility = 'visible';
+        }
+    }
+    updateWP();
+}
+
+function giveUber(tag) {
+    document.getElementById(tag + '-on').style.visibility = 'visible';
+    document.getElementById(tag + '-off').style.visibility = 'hidden';
+    document.getElementById(tag).onclick = function() {dropUber(tag);};
+    players[tag] += 1;
+    updateWP();
+}
+function dropUber(tag) {
+    document.getElementById(tag + '-on').style.visibility = 'hidden';
+    document.getElementById(tag + '-off').style.visibility = 'visible';
+    document.getElementById(tag).onclick = function() {giveUber(tag);};
+    players[tag] -= 1;
+    updateWP();
+}
+
+var xhr = new XMLHttpRequest();
+function updateWP() {
+    xhr.abort();
+
+    document.all.loading.style.visibility = 'visible';
+    var url = "http://connett.net/tf2/wp/backend.php?";
+
+    var sides = ["r","b"];
+    var positions = ["1","2","4","7","c"];
+
+    var s;
+    var i;
+    for (s = 0; s < sides.length; s++) {
+        for (i = 0; i < positions.length; i++) {
+            tag = sides[s] + positions[i];
+            url += tag + "=";
+            if (positions[i] == 'c' && players[sides[s] + '7'] == 0) {
+                // No ubercharge if medic is dead.
+                url += "0";
+            } else {
+                url += players[tag];
+            }
+            url += "&";
+        }
+    }
+
+    url += "map=" + escape(document.all.map[document.all.map.selectedIndex].value) + "&";
+    url += "point=" + point;
+
+    xhr.open("GET", url);
+    xhr.send()
+}
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        if (xhr.status = 200) {
+            document.all.xhrTarget.innerHTML = xhr.responseText;
+        } else {
+            document.all.xhrTarget.innerHTML = "<p>Error with XMLHttpRequest:</p>";
+            document.all.xhrTarget.innerHTML += xhr.statusText;
+        }
+        document.all.loading.style.visibility = 'hidden';
+    }
+}
+window.onload = updateWP;
